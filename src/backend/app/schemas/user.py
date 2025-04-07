dae31a70db0b92b362881fca98b595e085fb90b3
Breno Santana <breno.lima@sou.inteli.edu.br>
2025-04-07 14:45:34 -0300

    feat: schemas

---
+++ src/backend/app/schemas/user.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, EmailStr 
+from uuid import UUID
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    name: str   
+    email: EmailStr
+    password: str
+    role_id: UUID
+
+class UserResponse(BaseModel):
+    id: UUID
+    name: str
+    email: EmailStr
+    role_id: UUID
+    created_at: datetime
+    updated_at: datetime
+
+
+    class Config:
+        orm_mode = True # permite retornar os dados  diretamente de modelos SQLAlchemy
+
+        
