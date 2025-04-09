a724c310f7a50e9ce1efcb17ba143e952bec912d
SantanaBreno <breno.lima@sou.inteli.edu.br>
2025-04-09 16:45:43 -0300

    fix: clean connection.py

--- src/backend/app/db/connection.py
+++ src/backend/app/db/connection.py
@@ -6,17 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-# USER = os.getenv("user")
-# PASSWORD = os.getenv("password")
-# HOST = os.getenv("host")
-# PORT = os.getenv("port")
-# DBNAME = os.getenv("dbname")
-
-# db_url = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
-
-db_url = 
-
-# db_url = os.getenv("DB_URL")
+db_url = os.getenv("DB_URL")
 print(db_url)
 engine = create_engine(db_url, pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
