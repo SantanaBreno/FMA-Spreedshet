1512e90bf0029ec73d5586080e565d97940fd9c9
SantanaBreno <breno.lima@sou.inteli.edu.br>
2025-04-15 12:55:27 -0300

    feat: profile icon component

---
+++ src/frontend/src/pages/DecorationWorksheets/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const DecorationWorksheetsWrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  color: white;
+  padding: 0; // ou ajuste para não estourar a altura
+`;
